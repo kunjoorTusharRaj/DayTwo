c44c0ef9037c8ecfaaba1cacc7b94e0ea2bc7ae3
kunjoorTusharRaj <kunjoortusharraj@gmail.com>
2025-03-22 14:40:01 +0530

    updated four.java file

--- four.java
+++ four.java
@@ -1,5 +1,7 @@
 public class four {
     public static void main(String args[]) {
+        int data1=0;
+        int data2=0;
         System.out.println("hellow this is tushar");
         System.out.println("this is four");
     }
