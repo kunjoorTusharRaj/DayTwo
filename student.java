1325d641db9b36502f0b6d3d5715be3e236dbef7
kunjoorTusharRaj <kunjoortusharraj@gmail.com>
2025-03-22 15:12:59 +0530

    added some changes in studen.java

--- student.java
+++ student.java
@@ -2,5 +2,29 @@ public class student {
 
     public static void main(String[] args) {
         System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
+        System.out.println("this is the studen class");
     }
 }
