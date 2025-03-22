3198c369db518572a9f731c7f8be6b1622a513cd
kunjoorTusharRaj <kunjoortusharraj@gmail.com>
2025-03-22 18:23:00 +0530

    added the try catch block

--- evenorodd.java
+++ evenorodd.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 public class evenorodd {
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("enter a number ");
-        int num = s.nextInt();
-        if(num%2==0){
-            System.out.println("even");
-            return;
-        }else{
-            System.out.println("odd");
+        try {
+            Scanner s = new Scanner(System.in);
+            System.out.println("enter a number ");
+            int num = s.nextInt();
+            if(num%2==0){
+                System.out.println("even");
+                return;
+            }else{
+                System.out.println("odd");
+            }
+        } catch (Exception e) {
+            System.out.println("errot message "+e);
         }
     }
 }
