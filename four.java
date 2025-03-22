e353757fff02e06c89e36c9997f75ae75d41c296
kunjoorTusharRaj <kunjoortusharraj@gmail.com>
2025-03-22 14:51:17 +0530

    updated four.java

--- four.java
+++ four.java
@@ -1,6 +1,21 @@
 public class four {
     public static void main(String args[]) {
         System.out.println("hellow this is tushar");
+        System.out.println("hellow this is tushar");
+        System.out.println("hellow this is tushar");
+        System.out.println("hellow this is tushar");
+        System.out.println("hellow this is tushar");
+        System.out.println("hellow this is tushar");
+        System.out.println("hellow this is tushar");
+        System.out.println("hellow this is tushar");
+        System.out.println("hellow this is tushar");
+        System.out.println("hellow this is tushar");
+        System.out.println("hellow this is tushar");
+        System.out.println("hellow this is tushar");
+        System.out.println("hellow this is tushar");
+        System.out.println("hellow this is tushar");
+        System.out.println("hellow this is tushar");
+        System.out.println("hellow this is tushar");
         System.out.println("this is four");
     }
 }
